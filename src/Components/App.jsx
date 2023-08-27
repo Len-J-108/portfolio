e95ad215a3ad56db4d099f8444b036cce8739d4b
Lennart Jansen <lennart.official.jansen@gmail.com>
2023-08-27 20:30:07 +0200

    heading with background added

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,6 +1,12 @@
-import '../Styles/App.css';
+import '../Styles/app.scss';
+
+import HeadingWithBg from './HeadingWithBg';
 function App() {
-  return <div>App component</div>;
+  return (
+    <div className='app'>
+      <HeadingWithBg />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/HeadingWithBg.jsx
@@ -0,0 +1,11 @@
+import '../Styles/headingWithBg.scss';
+
+const HeadingWithBg = () => {
+  return (
+    <div className='text-container'>
+      <h2 className='text'>Lennart Jansen</h2>;
+    </div>
+  );
+};
+
+export default HeadingWithBg;
