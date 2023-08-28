651f43f58dbb03138419129e4e3aa93d1d5860d5
Lennart Jansen <lennart.official.jansen@gmail.com>
2023-08-28 21:52:27 +0200

    modded entry ani

--- src/Components/HeadingWithBg.jsx
+++ src/Components/HeadingWithBg.jsx
@@ -2,10 +2,14 @@ import '../Styles/headingWithBg.scss';
 
 const HeadingWithBg = () => {
   return (
-    <div className='text-container'>
-      <span className='text rotate-1'>Lennart</span>
-      <span className='text rotate-2'>Jansen</span>
-    </div>
+    <>
+      <div className='text-container'>
+        <span className='text rotate-1'>Lennart</span>
+        <span className='text rotate-2'>Jansen</span>
+      </div>
+      <div className="wheel-container"></div>
+      <div className='wheel'></div>
+    </>
   );
 };
 
