3fd67ad9ee213a6e1132b8b53728198eb1a27a60
Lennart Jansen <lennart.official.jansen@gmail.com>
2023-08-31 00:14:02 +0200

    started

--- src/Components/ContentLeft.jsx
+++ src/Components/ContentLeft.jsx
@@ -1,4 +1,6 @@
 import '../Styles/Components/ContentLeft.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PureCarousel from './PureCarousel';
 
 const ContentLeft = ({ color }) => {
   const style = {
@@ -7,41 +9,7 @@ const ContentLeft = ({ color }) => {
   };
   return (
     <div className='content-left-wrapper' style={style}>
-      <div
-        id='carouselExampleInterval'
-        class='carousel slide'
-        data-bs-ride='carousel'
-      >
-        <div class='carousel-inner'>
-          <div class='carousel-item active' data-bs-interval='10000'>
-            <img src='...' class='d-block w-100' alt='...' />
-          </div>
-          <div class='carousel-item' data-bs-interval='2000'>
-            <img src='...' class='d-block w-100' alt='...' />
-          </div>
-          <div class='carousel-item'>
-            <img src='...' class='d-block w-100' alt='...' />
-          </div>
-        </div>
-        <button
-          class='carousel-control-prev'
-          type='button'
-          data-bs-target='#carouselExampleInterval'
-          data-bs-slide='prev'
-        >
-          <span class='carousel-control-prev-icon' aria-hidden='true'></span>
-          <span class='visually-hidden'>Previous</span>
-        </button>
-        <button
-          class='carousel-control-next'
-          type='button'
-          data-bs-target='#carouselExampleInterval'
-          data-bs-slide='next'
-        >
-          <span class='carousel-control-next-icon' aria-hidden='true'></span>
-          <span class='visually-hidden'>Next</span>
-        </button>
-      </div>
+      <PureCarousel />
     </div>
   );
 };
