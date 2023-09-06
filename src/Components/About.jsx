ef39a0d4014c8793bbc8fc2cc21dcc3fe4902ddf
Lennart Jansen <lennart.official.jansen@gmail.com>
2023-09-06 22:24:48 +0200

    faceImage added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,36 +9,36 @@ const About = ({ switchProps }) => {
       exit='exit'
       className='bg-neutral-800 w-full h-full rounded-lg flex items-center flex-col gap-6 overflow-y-scroll'
     >
-      <h2 className='fff-bodoni text-5xl mt-10 pb-6  px-6 border-b-2 skew-x-6 skew-y-6'>
+      <h2 className='fff-bodoni sm:text-xs md:text-5xl lg:text-6xl mt-10 pb-6  px-6 border-b-2 skew-x-6 skew-y-6'>
         About me
       </h2>
       <div className='wrapper w-5/6'>
-        <p className='w-10/12 mx-5 my-12 text-center'>
+        <p className='w-10/12 mx-auto my-12 text-center md:text-lg lg:text-2xl font-thin'>
           Hi. My Name is Lennart Jansen from Hamburg. A have a background as an
           Audio Engeneer. Recently i discovered my passion with programming and
           web-development. After going the sel-taught route at first i decided
           to join a WebDev Course at DCI in january this year.
         </p>
-        <div id='life' className='flex flex-col h-96 justify-evenly text-lg'>
-          <div className='w-10/12 flex justify-between'>
+        <div id='life' className='flex flex-col w-8/12 h-96 mx-auto justify-evenly text-lg'>
+          <div className='w-full flex justify-between'>
             <h3 className='inline-block text-sky-300'>Until 2021:</h3>
-            <span className='font-thin '>Working as an Audio Engeneer</span>
+            <span className='font-thin'>Working as an Audio Engeneer</span>
           </div>
-          <div className='w-10/12 flex justify-between'>
+          <div className='w-full flex justify-between'>
             <h3 className='inline-block text-sky-300'>2022:</h3>
             <span className='font-thin '>
-              Startet learning on FreeCodeCamp & CodeCademy{' '}
+              Startet learning on FreeCodeCamp
             </span>
           </div>
-          <div className='w-10/12 flex justify-between'>
+          <div className='w-full flex justify-between'>
             <h3 className='inline-block text-sky-300'>2023:</h3>
             <span className='font-thin '>Studying Web Development at DCI</span>
           </div>
-          <div className='w-10/12 flex justify-between'>
+          <div className='w-full flex justify-between'>
             <h3 className='inline-block text-sky-300'>Age: 38</h3>
             <span className='font-thin '>Located: Hamburg / Germany</span>
           </div>
-          <div className='w-10/12 flex justify-between'>
+          <div className='w-full flex justify-between'>
             <h3 className='inline-block text-sky-300'>Languages:</h3>
             <ul className='font-thin'>
               <li>English (fluently)</li>
--- src/Components/ContentLeftBottom.jsx
+++ src/Components/ContentLeftBottom.jsx
@@ -4,14 +4,17 @@ import { VscGithub } from 'react-icons/vsc';
 import { SiLinkedin } from 'react-icons/si';
 import { BsDiscord } from 'react-icons/bs';
 
+import faceImage from '../Assets/images/faceRoundScaledDown.png';
+
 const ContentLeftBottom = () => {
   const twClassNames = ['hidden', 'lg:block', 'backdrop-blur-md'];
   return (
     <div className={twClassNames.join(' ') + ' content-left-bottom'}>
-      <div className='flex h-full justify-end items-end p-3 gap-3'>
-        <div className='text-black self-start absolute left-0 rounded-full border border-black w-16 h-16'>
-          Photo
-        </div>
+      <div className='flex h-full justify-end items-end p-3 gap-3 relative'>
+        {/* <div className='text-black self-start relative left-0 rounded-full border border-black w-16 h-16'>
+          Photo */}
+          <img src={faceImage} alt="face" className="face-round w-32 h-32 object-cover border-4 border-red-900 drop-shadow rounded-full grayscale self-start" />
+        {/* </div> */}
         <div
           id='heading-name'
           className='fff-bodoni self-center absolute left-6 bottom-40 '
@@ -19,7 +22,7 @@ const ContentLeftBottom = () => {
           <h5 className='text-3xl text-neutral-800'>Lennart</h5>
           <h5 className='text-3xl text-neutral-800 relative left-10'>Jansen</h5>
         </div>
-        <div id='heading-decrition' className='self-center relative top-20'>
+        <div id='heading-decrition' className='self-center relative top-14'>
           <h5 className='fff-bodoni  text-2xl text-neutral-800'>Frontend</h5>
           <h5 className='fff-bodoni  text-2xl text-neutral-800 relative left-10'>
             Developer
